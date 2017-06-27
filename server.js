750bda977a6fd7fd3fbd856858053341d462df21
supachai.thongprasit <supachai.thongprasit@gmail.com>
2017-06-27 17:00:32 +0700

    fix header

--- server.js
+++ server.js
@@ -1,52 +1,49 @@
-const express = require('express')
-const hbs = require('hbs')
-const fs = require('fs')
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials');
 
-app.set('view engine','hbs')
+app.set('view engine','hbs');
 
 app.use((req,res,next)=>{
     var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`
-    console.log(log)
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
     fs.appendFile('server.log',log+'\n',(err)=>{
         if(err){
-            console.log('Unable to append to server.log')
+            console.log('Unable to append to server.log');
         }
     })
-    next()
-})
+    next();
+});
 
-// app.use((req,res,next)=>{
-//     res.render('maintenance.hbs')
-// })
-
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/public'));
 
 hbs.registerHelper('getCurrentYear',()=>{
-    return new Date().getFullYear()
+    return new Date().getFullYear();
 })
 
 hbs.registerHelper('screamIt',(text)=>{
-    return text.toUpperCase()
+    return text.toUpperCase();
 })
 
 app.get('/',(req,res) =>{
     res.render('home.hbs',{
         pageTitle:'Home Page',
         welcomeMessage:'Welcome to my WebSite'
-    })
+    });
 })
 
 app.get('/about',(req,res) =>{
     res.render('about.hbs',{
         pageTitle:'About Page'
-    })
+    });
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up')
+app.listen(port, ()=>{
+    console.log(`Server is up on port: ${port}`);
 })
